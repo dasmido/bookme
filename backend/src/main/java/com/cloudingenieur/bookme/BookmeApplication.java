9c87cf82ad7acf66d7cc456670912da785dab3e9
Mohammed Jamal <mohammed.jamalhadi@gmail.com>
2024-01-17 00:07:30 +0100

    initial commit

---
+++ backend/src/main/java/com/cloudingenieur/bookme/BookmeApplication.java
@@ -0,0 +1,13 @@
+package com.cloudingenieur.bookme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookmeApplication.class, args);
+	}
+
+}
